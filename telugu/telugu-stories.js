fd485de25f72cb4030e3f72e72c36fd56c256afd
lustnote <lustnote08@gmail.com>
2025-12-21 22:04:15 +0530

    Create telugu-stories.js
    Signed-off-by: lustnote <lustnote08@gmail.com>

---
+++ telugu/telugu-stories.js
@@ -0,0 +1,37 @@
+const stories = [
+  {
+    id: 1,
+    title: "Pakkinti aunty puku pagala dengina roju",
+    desc: "naa pakkkintk aunty puku pagala dengina story.",
+    link: "/Lustnote/telugu/story1.html",
+    category: "aunty",
+    date: "2025-12-16"
+  },
+  {
+    id: 2,
+    title: "nenu na girl friend part 1",
+    desc: "nenu na girl friend tho chesina wild sex part 1",
+    link: "/Lustnote/telugu/story2-telugu.html",
+    category: "girlfirend",
+    date: "2025-12-17"
+  },
+
+  {
+    id: 3,
+    title: "nenu na girl friend part 2",
+    desc: "nenu na girl friend tho chesi wild sex part 2",
+    link: "/Lustnote/telugu/story3.html",
+    category: "girlfriend",
+    date: "2025-12-20"
+  }
+  
+  {
+    id: 3, 
+    title: "sruthi akka tho sankrathi",
+    desc: "naa akka peru sruthi sankrathi festival lo kukala dengina dhanini",
+    link: "/Lustnote/telugu/story4-telugu-akka-thamudu.html",
+    category: "akka",
+    date: "2025-12-21"
+  }
+  
+];
