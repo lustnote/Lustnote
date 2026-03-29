0cde740bec6c8fa8521238b52a584a694cb44ffe
lustnote <lustnote08@gmail.com>
2026-03-29 08:24:52 +0530

    Create ARS.js
    Signed-off-by: lustnote <lustnote08@gmail.com>

---
+++ ARS.js
@@ -0,0 +1,41 @@
+[
+(function(){
+  const box = document.getElementById("suggestionsBox");
+  if(!box || typeof stories === "undefined") return;
+
+  const currentUrl = window.location.pathname;
+
+  // current story find
+  let currentStory = stories.find(s => currentUrl.includes(s.link));
+
+  if(!currentStory) return;
+
+  // same category + same language filter
+  let related = stories.filter(s => 
+    s.category === currentStory.category &&
+    s.language === currentStory.language &&
+    s.link !== currentStory.link
+  );
+
+  // if less than 3, fill with latest
+  if(related.length < 3){
+    let extra = stories.filter(s => s.link !== currentStory.link);
+    related = [...related, ...extra];
+  }
+
+  // random 4 select
+  related = related.sort(() => 0.5 - Math.random()).slice(0,4);
+
+  // render cards
+  related.forEach(story => {
+    box.innerHTML += `
+      <a href="${story.link}" class="suggest-card">
+        <span class="suggest-tag">${story.category}</span>
+        <h4>${story.title}</h4>
+        <p>${story.desc.substring(0,60)}...</p>
+      </a>
+    `;
+  });
+
+})();
+]
