6aa9ca1ef9a0f97ccd8929cb1bede844ab6971df
lustnote <lustnote08@gmail.com>
2026-06-26 14:59:10 +0530

    Update stories.js
    Signed-off-by: lustnote <lustnote08@gmail.com>

--- stories.js
+++ stories.js
@@ -335,6 +335,16 @@ const stories = [
     category: "vadhina",
     language: "telugu",
     date: "2026-05-06"
+  },
+
+      {
+    id: 34, 
+    title: "Pkkinti Aunty ki na virginity echi sukhapeta.",
+    desc: "Pakkinnti lo unde aunty tho nen ela chesa. Aunty naku sex lo sukham chupinchi ela thana korika teerchukuni sukha padindho ee katha lo rasa.",
+    link: "/Lustnote/telugu/aunty/pakkinti-ki-na-virginity-echa.html.html",
+    category: "aunty",
+    language: "telugu",
+    date: "2026-06-23"
   }
 
   
