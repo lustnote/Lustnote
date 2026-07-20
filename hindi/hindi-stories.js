164f0a2f8e72efe08ef7b87886fa1067048198b6
Lets earn money <lustnote08@gmail.com>
2026-07-20 19:01:46 +0530

    Update hindi-stories.js
    Signed-off-by: Lets earn money  <lustnote08@gmail.com>

--- hindi/hindi-stories.js
+++ hindi/hindi-stories.js
@@ -1,1 +1,22 @@
-1
+const stories = [
+  {
+    id: 1,
+    title: "Ye dosti hum nahi todenge-1",
+    desc: "Ye kahani meri aur meri best friend Chikoo ki hai.",
+    link: "/hindi/hindi-story1.html",
+    category: "dosth",
+    language: "hindi",
+    date: "2025-12-22"
+  },
+
+  {
+    id: 2, 
+    title: "Tanvi madam chudai hindi story 2",
+    desc: "corporate company tanvi madam chudai.",
+    link: "/hindi/tanvi-madam-hindi-story2.html",
+    category: "aunty",
+    language: "hindi",
+    date: "2026-04-15"
+    }
+
+  ];
